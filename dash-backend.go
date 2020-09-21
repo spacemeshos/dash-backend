deb332e31643c8345d6f511331e183fbe32e73d0
Andrii Rozinko <a.rozinko@minerall.io>
2020-09-21 11:35:44 +0300

    Upgraded main

--- dash-backend.go
+++ dash-backend.go
@@ -19,6 +19,8 @@ var (
 
     nodeAddress = flag.String("node", "localhost:9092", "api node address")
     wsAddr = flag.String("ws", ":8080", "http service address")
+    mongoDbUrlStringFlag = flag.String("mongodb", "mongodb://localhost:27017", "Explorer MongoDB Uri string in format mongodb://<host>:<port>")
+    mongoDbNameStringFlag = flag.String("db", "explorer", "MongoDB Explorer database name string")
 )
 
 func main() {
@@ -31,16 +33,21 @@ func main() {
     bus := client.NewBus()
     go bus.Run()
 
-    history := history.NewHistory(bus)
+    history, err := history.NewHistory(nil, bus, *mongoDbUrlStringFlag, *mongoDbNameStringFlag)
+    if err != nil {
+        log.Error("Create History service error: %v", err)
+        return
+    }
     go history.Run()
 
     http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
         client.ServeWs(bus, w, r)
     })
 
-    err := http.ListenAndServe(*wsAddr, nil)
+    err = http.ListenAndServe(*wsAddr, nil)
     if err != nil {
-        log.Error("ListenAndServe: ", err)
+        log.Error("Create HTTP ssrver error: %v", err)
+        return
     }
 }
 
