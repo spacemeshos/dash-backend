9bddaf39c70267acfdec7e7a02b1b2a1a282d862
Kacper Sawicki <kacper@ksawicki.pl>
2023-01-23 18:20:20 +0100

    Bump CLI to v2 and update mongo driver

--- dash-backend.go
+++ dash-backend.go
@@ -1,125 +1,124 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "io"
-    "net/http"
-    "math/rand"
-    "os"
-    "time"
+	"context"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 
-    "github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 
-    "github.com/spacemeshos/go-spacemesh/log"
+	"github.com/spacemeshos/go-spacemesh/log"
 
-    "github.com/spacemeshos/dash-backend/client"
-    "github.com/spacemeshos/dash-backend/history"
+	"github.com/spacemeshos/dash-backend/client"
+	"github.com/spacemeshos/dash-backend/history"
 )
 
 var (
-    version string
-    commit  string
-    branch  string
+	version string
+	commit  string
+	branch  string
 )
 
 var (
-    listenStringFlag      string
-    mongoDbUrlStringFlag  string
-    mongoDbNameStringFlag string
+	listenStringFlag      string
+	mongoDbUrlStringFlag  string
+	mongoDbNameStringFlag string
 )
 
 var flags = []cli.Flag{
-    cli.StringFlag{
-        Name:        "listen",
-        Usage:       "Dashboar API listen string in format <host>:<port>",
-        Required:    false,
-        Destination: &listenStringFlag,
-        Value:       ":8080",
-    },
-    cli.StringFlag{
-        Name:        "mongodb",
-        Usage:       "Explorer MongoDB Uri string in format mongodb://<host>:<port>",
-        Required:    false,
-        Destination: &mongoDbUrlStringFlag,
-        Value:       "mongodb://localhost:27017",
-    },
-    cli.StringFlag{
-        Name:        "db",
-        Usage:       "MongoDB Explorer database name string",
-        Required:    false,
-        Destination: &mongoDbNameStringFlag,
-        Value:       "explorer",
-    },
+	&cli.StringFlag{
+		Name:        "listen",
+		Usage:       "Dashboar API listen string in format <host>:<port>",
+		Required:    false,
+		Destination: &listenStringFlag,
+		Value:       ":8080",
+	},
+	&cli.StringFlag{
+		Name:        "mongodb",
+		Usage:       "Explorer MongoDB Uri string in format mongodb://<host>:<port>",
+		Required:    false,
+		Destination: &mongoDbUrlStringFlag,
+		Value:       "mongodb://localhost:27017",
+	},
+	&cli.StringFlag{
+		Name:        "db",
+		Usage:       "MongoDB Explorer database name string",
+		Required:    false,
+		Destination: &mongoDbNameStringFlag,
+		Value:       "explorer",
+	},
 }
 
 func main() {
-    app := cli.NewApp()
-    app.Name = "Spacemesh Dashboard API Server"
-    app.Version = fmt.Sprintf("%s, commit '%s', branch '%s'", version, commit, branch)
-    app.Flags = flags
-    app.Writer = os.Stderr
+	app := cli.NewApp()
+	app.Name = "Spacemesh Dashboard API Server"
+	app.Version = fmt.Sprintf("%s, commit '%s', branch '%s'", version, commit, branch)
+	app.Flags = flags
+	app.Writer = os.Stderr
 
-    app.Action = func(ctx *cli.Context) (error) {
+	app.Action = func(ctx *cli.Context) error {
 
-        rand.Seed(time.Now().UTC().UnixNano())
+		rand.Seed(time.Now().UTC().UnixNano())
 
-        env, ok := os.LookupEnv("SPACEMESH_API_LISTEN")
-        if ok {
-            listenStringFlag = env
-        }
-        env, ok = os.LookupEnv("SPACEMESH_MONGO_URI")
-        if ok {
-            mongoDbUrlStringFlag = env
-        }
-        env, ok = os.LookupEnv("SPACEMESH_MONGO_DB")
-        if ok {
-            mongoDbNameStringFlag = env
-        }
+		env, ok := os.LookupEnv("SPACEMESH_API_LISTEN")
+		if ok {
+			listenStringFlag = env
+		}
+		env, ok = os.LookupEnv("SPACEMESH_MONGO_URI")
+		if ok {
+			mongoDbUrlStringFlag = env
+		}
+		env, ok = os.LookupEnv("SPACEMESH_MONGO_DB")
+		if ok {
+			mongoDbNameStringFlag = env
+		}
 
-        bus := client.NewBus()
-        go bus.Run()
+		bus := client.NewBus()
+		go bus.Run()
 
-        history, err := history.NewHistory(nil, bus, mongoDbUrlStringFlag, mongoDbNameStringFlag)
-        if err != nil {
-            log.Error("Create History service error: %v", err)
-            return err
-        }
-        go history.Run()
+		history, err := history.NewHistory(nil, bus, mongoDbUrlStringFlag, mongoDbNameStringFlag)
+		if err != nil {
+			log.Error("Create History service error: %v", err)
+			return err
+		}
+		go history.Run()
 
-        http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-            client.ServeWs(bus, w, r)
-        })
+		http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+			client.ServeWs(bus, w, r)
+		})
 
-        http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-            w.Header().Set("Content-Type", "text/plain")
-            w.Header().Set("Access-Control-Allow-Origin", "*")
+		http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "text/plain")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 
-            networkInfo, err := history.GetStorage().GetNetworkInfo(context.Background())
-            if err == nil && networkInfo.IsSynced {
-                w.WriteHeader(http.StatusOK)
-                io.WriteString(w, "SYNCED")
-            } else {
-                w.WriteHeader(http.StatusTooEarly)
-                io.WriteString(w, "SYNCING")
-            }
-        })
+			networkInfo, err := history.GetStorage().GetNetworkInfo(context.Background())
+			if err == nil && networkInfo.IsSynced {
+				w.WriteHeader(http.StatusOK)
+				io.WriteString(w, "SYNCED")
+			} else {
+				w.WriteHeader(http.StatusTooEarly)
+				io.WriteString(w, "SYNCING")
+			}
+		})
 
-        err = http.ListenAndServe(listenStringFlag, nil)
-        if err != nil {
-            log.Error("Create HTTP ssrver error: %v", err)
-            return err
-        }
+		err = http.ListenAndServe(listenStringFlag, nil)
+		if err != nil {
+			log.Error("Create HTTP ssrver error: %v", err)
+			return err
+		}
 
-        log.Info("Server is shutdown")
-        return nil
-    }
+		log.Info("Server is shutdown")
+		return nil
+	}
 
-    if err := app.Run(os.Args); err != nil {
-        log.Info("%v", err)
-        os.Exit(1)
-    }
+	if err := app.Run(os.Args); err != nil {
+		log.Info("%v", err)
+		os.Exit(1)
+	}
 
-    os.Exit(0)
+	os.Exit(0)
 }
-
--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/spacemeshos/explorer-backend v0.0.13
 	github.com/spacemeshos/go-spacemesh v0.2.19-beta.0
-	github.com/urfave/cli v1.22.4
-	go.mongodb.org/mongo-driver v1.10.1
+	github.com/urfave/cli/v2 v2.23.7
+	go.mongodb.org/mongo-driver v1.11.1
 )
